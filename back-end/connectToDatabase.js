310ee90ccb1e224b85bf79c105eeb0bed69a5f8c
Agbley <agbleyjill@gmail.com>
2023-12-23 13:00:43 +0000

    mongodb commit

--- back-end/connectToDatabase.js
+++ back-end/connectToDatabase.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const connectToDatabase = async () => {
   // Connects to the Mongo DB database
   try {
-    await mongoose.connect("mongodb://localhost:27017/scaler");
+    await mongoose.connect(
+      "mongodb+srv://mawudev:mawudev@mawudev.ygbfjjk.mongodb.net/?retryWrites=true&w=majority"
+    );
     console.log("Connected to the database");
   } catch (error) {
     console.error("Error connecting to the database:", error.message);
